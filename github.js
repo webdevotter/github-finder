9ca84b95063a901afaa4b31307011fa42658b755
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-11-23 11:20:46 -0600

    Github Finder Part 5
    Fetch & Display Repos

--- github.js
+++ github.js
@@ -1,16 +1,22 @@
 class GitHub {
   constructor() {
     this.client_id = '04f806582c0dbba0ed18';
-    this.client_secret = 'bce83e816c78d856b7380a8edbaddf2e7deec77f';
+    this.client_secret = '94da980c0b54b30575fa4fb8cb2ed931ac9e2dbb';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     return {
-      profile
+      profile,
+      repos
     }
   }
 }
--- ui.js
+++ ui.js
@@ -2,7 +2,7 @@ class UI {
   constructor() {
     this.profile = document.getElementById('profile');
   }
-
+  // Display profile in ui
   showProfile(user) {
     this.profile.innerHTML = `
       <div class="card card-body mb-3">
@@ -29,5 +29,67 @@ class UI {
       <h3 class="page-heading mb-3">Latest Repos</h3>
       <div id="repos"></div>
     `;
+  }
+
+  // Show your repos
+  showRepos(repos) {
+    let output = '';
+
+    repos.forEach(function(repo) {
+      output += `
+        <div class="card card-body mb-2">
+          <div class="row">
+            <div class="col-md-6">
+              <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+            </div>
+            <div class="col-md-6">
+            <span class="badge badge-primary">Stars: ${repo.stargazers_count}</span>
+            <span class="badge badge-secondary">Watchers: ${repo.watchers_count}</span>
+            <span class="badge badge-success">Forks: ${repo.forks_count}</span>
+            </div>
+          </div>
+        </div>
+      `;
+    });
+
+    // Output repos
+
+    document.getElementById('repos').innerHTML = output;
+  }
+
+  // Show alert message
+  showAlert(message, className) {
+    // Clear any remaining alerts
+    this.clearAlert();
+    // Create div
+    const div = document.createElement('div');
+    // Add classes
+    div.className = className;
+    // Add text
+    div.appendChild(document.createTextNode(message));
+    // Get parent
+    const container = document.querySelector('.searchContainer');
+    // Get search box
+    const search = document.querySelector('.search');
+    // Insert alert
+    container.insertBefore(div, search);
+
+    // Timeout after 3 seconds
+    setTimeout(() => {
+      this.clearAlert();
+    }, 3000);
+  }
+
+  // Clear alert message
+  clearAlert() {
+    const currentAlert = document.querySelector('.alert');
+    if(currentAlert) {
+      currentAlert.remove();
+    }
+  }
+
+  // Clear profile
+  clearProfile() {
+    this.profile.innerHTML = '';
   }
 }
